6bac499cbbf7f39b20187fbf76cd9cb71bb2e9ec
DESKTOP-8L5OUK3\LENOVO <tousifjamadar52a@gmail.com>
2024-11-06 21:38:21 +0530

    Dialog Box Added

---
+++ script.js
@@ -0,0 +1,8 @@
+function html(){
+    const overlayElement = document.getElementById('overlay');
+    overlayElement.style.display = 'flex';
+}
+function end(){
+    const overlayElement = document.getElementById('overlay');
+    overlayElement.style.display = 'none';
+}
